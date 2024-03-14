cd4a4011519e369eb7b963bfcbd38b85130baf74
Ajarek <ajarek@poczta.onet.pl>
2024-03-15 00:31:54 +0100

    added onSubmit put

--- frontend/src/pages/Edit/Edit.jsx
+++ frontend/src/pages/Edit/Edit.jsx
@@ -6,7 +6,17 @@ const Edit = () => {
   let { id } = useParams()
   const [data, setData] = useState({})
   const onSubmit = async (data) => {
-    
+    try {
+      const response = await fetch(`http://localhost:3000/api/products/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      });
+      const updatedData = await response.json();
+      console.log(updatedData);
+    } catch (error) {
+      console.error(error);
+    } 
   }
   useEffect(() => {
     const dataFetch = async () => {

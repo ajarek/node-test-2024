87c4c4968190c8cb453976e37652053e8927da3c
Ajarek <ajarek@poczta.onet.pl>
2024-03-14 22:59:00 +0100

    added AddProduct

--- frontend/src/pages/AddProduct/AddProduct.jsx
+++ frontend/src/pages/AddProduct/AddProduct.jsx
@@ -1,10 +1,23 @@
-import {Form} from '../../components/Form/Form'
+import { Form } from '../../components/Form/Form'
 import './AddProduct.css'
 const AddProduct = () => {
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch('http://localhost:3000/api/products', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      })
+      const createdItem = await response.json()
+      console.log(createdItem)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
-    <div className="add">
+    <div className='add'>
       <h1>Add Product</h1>
-      <Form/>
+      <Form onSubmit={onSubmit} />
     </div>
   )
 }
